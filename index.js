9770bb615944303c5dd3dde522051230b2c8298c
masum691 <masumhu691@gmail.com>
2021-10-29 16:21:34 +0600

    dotenv

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icwz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
 
 app.get('/', (req, res) => {
     res.send('World travel server');
@@ -14,3 +21,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Word travel listening on the port', port)
 })
+
